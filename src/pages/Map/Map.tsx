cc66bde7f5fd957ffec3ac831460971279ba8d70
성진옥 <wlsdhr0831@naver.com>
2023-01-02 23:45:45 +0900

    feat: 지도 페이지 제작

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import Search from "./components/Search";
 import MapView from "./Map.view";
 
 const Map = () => {
-  return <MapView />;
+  const [position, setPosition] = useState<{ lat: number; lng: number }>();
+
+  return (
+    <>
+      <Search setPosition={setPosition} />
+      <MapView {...position} />
+    </>
+  );
 };
 
 export default Map;
