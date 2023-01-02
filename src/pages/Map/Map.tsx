aa90c10aade5ef7ef0622ddac57b286348344e0c
성진옥 <wlsdhr0831@naver.com>
2023-01-03 02:48:21 +0900

    feat: 지도 헤더 추가

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
 import Search from "./components/Search";
 import MapView from "./Map.view";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { MdGpsFixed } from "react-icons/md";
+
+import styles from "./Map.module.scss";
+import classnames from "classnames/bind";
+import { useNavigate } from "react-router-dom";
+
+const cx = classnames.bind(styles);
 
 const Map = () => {
   const [position, setPosition] = useState<{ lat: number; lng: number }>();
+  const navigate = useNavigate();
 
   return (
     <>
+      <header className={cx("header")}>
+        <IoMdArrowRoundBack
+          className={cx("button")}
+          onClick={() => navigate(-1)}
+        />
+        <div className={cx("divider")} />
+        <MdGpsFixed className={cx("button")} onClick={() => navigate(0)} />
+      </header>
       <Search setPosition={setPosition} />
       <MapView {...position} />
     </>
