a9f3033b00408e69eee619da0a2dde198bb767ca
성진옥 <wlsdhr0831@naver.com>
2023-01-03 01:23:32 +0900

    feat: 메인 화면 제작

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,22 @@
 import MainView from "./Main.view";
 
+import styles from "./Main.module.scss";
+import classnames from "classnames/bind";
+
+const cx = classnames.bind(styles);
+
 const Main = () => {
-  return <MainView />;
+  return (
+    <div className={cx("background")}>
+      <header className={cx("header")}>
+        <div className={cx("logo")}>방방콕콕</div>
+        <div>
+          <button className={cx("login")}>로그인</button>
+        </div>
+      </header>
+      <MainView />
+    </div>
+  );
 };
 
 export default Main;
