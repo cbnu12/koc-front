9e8c40008e2d9573008e248b81227b9b533197e0
성진옥 <wlsdhr0831@naver.com>
2023-02-02 17:23:55 +0900

    feat: 외부선 케이스 추가

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -9,15 +9,17 @@ type Props = {
   value?: string;
   onChange?: () => void;
   placeholder?: string;
+  border?: boolean;
 };
 
 const Search = ({
   value,
   onChange,
   placeholder = "지도를 검색해보세요",
+  border,
 }: Props) => {
   return (
-    <div className={cx("container")}>
+    <div className={cx("container", border && "border")}>
       <input placeholder={placeholder} value={value} onChange={onChange} />
       <BiSearchAlt className={cx("icon")} />
     </div>
