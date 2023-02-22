56dbb83c0dabf2bebf02182bf8d6414328df79ea
성진옥 <wlsdhr0831@naver.com>
2023-02-22 21:29:44 +0900

    feat: Header 공통 컴포넌트 추출 및 적용

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import SearchAddress from "./components/SearchAddress";
 import MapView from "./Map.view";
-import { IoMdArrowRoundBack } from "react-icons/io";
-import { MdGpsFixed } from "react-icons/md";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { BiSearchAlt } from "react-icons/bi";
 import queryString from "query-string";
+import { Place } from "../../common/types";
+import Header from "../../common/componenents/Header";
 
 import styles from "./Map.module.scss";
 import classnames from "classnames/bind";
 import "./marker.css";
-import { Place } from "./types";
 
 const cx = classnames.bind(styles);
 
@@ -20,24 +19,18 @@ const Map = () => {
 
   const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number }>();
   const [markerList, setMarkerList] = useState<Place[]>([]);
-  const navigate = useNavigate();
   const [showSearch, setShowSearch] = useState<boolean>(!!keyword);
 
   return (
     <>
-      <header className={cx("header")}>
-        <IoMdArrowRoundBack
-          className={cx("button")}
-          onClick={() => navigate(-1)}
-        />
-        <div className={cx("divider")} />
-        <MdGpsFixed className={cx("button")} onClick={() => navigate(0)} />
-        <div className={cx("divider")} />
-        <BiSearchAlt
-          className={cx("button")}
-          onClick={() => setShowSearch(!showSearch)}
-        />
-      </header>
+      <Header
+        additionalIcons={[
+          <BiSearchAlt
+            className={cx("button")}
+            onClick={() => setShowSearch(!showSearch)}
+          />,
+        ]}
+      />
       {showSearch && (
         <SearchAddress
           setMapCenter={setMapCenter}
