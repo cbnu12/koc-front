db1108dbcdaf2575635c75a2ae4091ee6e253ef7
성진옥 <wlsdhr0831@naver.com>
2023-03-03 23:40:15 +0900

    feat: 메인 화면에 코스 섹션 추가

--- src/pages/Main/Main.view.tsx
+++ src/pages/Main/Main.view.tsx
@@ -79,6 +79,13 @@ const MainView = ({}: Props) => {
         direction="column"
         path="/theme/detail"
       />
+      <Section
+        title="인기있는 코스"
+        description="코스 따라 여행해보세요."
+        itemList={HotPlaceList}
+        direction="column"
+        path="/course/detail"
+      />
     </div>
   );
 };
