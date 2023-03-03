d7f928193c3b87f96abac06b41b33c391c159986
성진옥 <wlsdhr0831@naver.com>
2023-03-03 23:21:54 +0900

    feat: 지도 범위 수정

--- src/pages/ThemeDetail/Map.view.tsx
+++ src/pages/ThemeDetail/Map.view.tsx
@@ -50,6 +50,8 @@ const MapView = ({
       });
 
       if (markerList) {
+        let bounds = new kakao.maps.LatLngBounds();
+
         markerList.forEach((marker) => {
           const markerElement = document.createElement("div");
 
@@ -58,15 +60,21 @@ const MapView = ({
           markerElement.onclick = () => {
             onClickMarker(marker);
           };
+
+          const position = new kakao.maps.LatLng(marker.y, marker.x);
           const customOverlay = new kakao.maps.CustomOverlay({
-            position: new kakao.maps.LatLng(marker.y, marker.x),
+            position,
             content: markerElement,
             yAnchor: 0,
             clickable: true,
           });
 
+          bounds.extend(position);
+
           customOverlay.setMap(map);
         });
+
+        map.setBounds(bounds, 50, 50, 50, 50);
       } else {
         markerPoint.setMap(map);
       }
