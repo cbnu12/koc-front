3bf1e9ca62d6414478f391926801a1e467371d81
성진옥 <wlsdhr0831@naver.com>
2023-03-24 19:51:00 +0900

    feat: 모달 컴포넌트 추가

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,43 @@
+import styles from "./Modal.module.scss";
+import classnames from "classnames/bind";
+const cx = classnames.bind(styles);
+
+type Props = {
+  isShow: boolean;
+  title?: string;
+  description?: string;
+  useDim?: boolean;
+  children?: React.ReactNode;
+  onClose?: () => void;
+};
+
+const Modal = ({
+  onClose,
+  isShow,
+  useDim,
+  title,
+  description,
+  children,
+}: Props) => {
+  if (!isShow) {
+    return <></>;
+  }
+
+  return (
+    <>
+      {useDim && <div className={cx("dim")} />}
+      <div className={cx("container")}>
+        {onClose && (
+          <button onClick={onClose} type="button" className={cx("closeButton")}>
+            닫기
+          </button>
+        )}
+        {title && <div className={cx("title")}>{title}</div>}
+        {description && <div className={cx("description")}>{description}</div>}
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Modal;
