0965d043648663c8add0595689279c60ebc881cf
성진옥 <wlsdhr0831@naver.com>
2023-04-16 23:26:08 +0900

    feat: env 적용

--- src/common/apis/urls.ts
+++ src/common/apis/urls.ts
@@ -1,11 +1,11 @@
-// const BASE_URL = "http://121.167.132.221:9090";
-const BASE_URL = "";
-
 const apiUrls = {
   home: {
-    getHotCourseList: () => `${BASE_URL}/home/hot-courses`,
-    getRecommendThemeList: () => `${BASE_URL}/home/recommend-themes`,
-    getTrendPlaceList: () => `${BASE_URL}/home/trend-places`,
+    getHotCourseList: () =>
+      `${process.env.REACT_APP_BASE_URL}/home/hot-courses`,
+    getRecommendThemeList: () =>
+      `${process.env.REACT_APP_BASE_URL}/home/recommend-themes`,
+    getTrendPlaceList: () =>
+      `${process.env.REACT_APP_BASE_URL}/home/trend-places`,
   },
 };
 
