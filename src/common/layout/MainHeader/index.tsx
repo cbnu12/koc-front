a9434ba2736d1809e7dbf947690f338efd3f8d87
성진옥 <wlsdhr0831@naver.com>
2023-04-29 13:51:49 +0900

    feat: 로그인 모달 임시 적용

--- src/common/layout/MainHeader/index.tsx
+++ src/common/layout/MainHeader/index.tsx
@@ -1,18 +1,35 @@
 import styles from "./MainHeader.module.scss";
 import classnames from "classnames/bind";
+import { useState } from "react";
+import Modal from "../../../components/Modal";
 
 const cx = classnames.bind(styles);
 
 const MainHeader = () => {
+  const [showLoginModal, setLoginModal] = useState<boolean>(false);
+
   return (
-    <header className={cx("header")}>
-      <div className={cx("logo")}>방방콕콕</div>
-      <div>
-        <button className={cx("login")} onClick={() => alert("로그인")}>
-          로그인
-        </button>
-      </div>
-    </header>
+    <>
+      <header className={cx("header")}>
+        <div className={cx("logo")}>방방콕콕</div>
+        <div>
+          <button className={cx("login")} onClick={() => setLoginModal(true)}>
+            로그인
+          </button>
+        </div>
+      </header>
+      <Modal
+        isShow={showLoginModal}
+        useDim
+        onClose={() => setLoginModal(false)}
+      >
+        <div className={cx("loginModal")}>
+          TODO: 디자인 로그인하고 모든 기능을 이용해보세요. 필요한 시간은 단,
+          3초!
+          <button>카카오톡으로 로그인하기</button>
+        </div>
+      </Modal>
+    </>
   );
 };
 
